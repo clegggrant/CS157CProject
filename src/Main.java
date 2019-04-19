fad1c791257eb26d8154258d556788aa4cb5b7ed
Grant <clegggrant@gmail.com>
2019-04-19 12:01:30 -0700

    Experimenting with auto formatter

--- src/Main.java
+++ src/Main.java
@@ -22,64 +22,64 @@ import java.util.logging.Logger;
 import java.util.Scanner;
 
 public class Main {
-	
-	private void count() {
-		MongoClientURI connectionString = new MongoClientURI("mongodb://ec2-13-59-38-216.us-east-2.compute.amazonaws.com:27018");
-		MongoClient mongo = new MongoClient(connectionString);
-		MongoDatabase database = mongo.getDatabase("testdb");
-		MongoCollection<Document> collection = database.getCollection("testCol");
-		System.out.println("Collection " + 
-				collection.getNamespace().getDatabaseName() + 
-				"." + 
-				collection.getNamespace().getCollectionName() + 
-				" has " +
-				collection.countDocuments() + 
-				" records.");
-		mongo.close();
-	}
-	
-	public static void main(String[] args) {
-		Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
-		mongoLogger.setLevel(Level.SEVERE);
-		
-		Main main = new Main();
-		
-		System.out.println("Welcome to the Employee Compensation Console\n\n"
-				+ "Version: 0.1\n\n"
-				+ "Enter help for list of commands\n");
-		
-		Scanner s = new Scanner(System.in);
-		
-		while(true) {
-			System.out.print("command> ");
-			String input = s.next();
-			if(input.toLowerCase().trim().equals("help")) {
-				System.out.println("exit - exit console");
-				System.out.println("count - return the number of records on file");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-				System.out.println("? - ");
-			}
-			else if (input.toLowerCase().trim().equals("exit"))
-				break;
-			else if(input.toLowerCase().trim().equals("count")) {
-				main.count();
-			}
-			else {
-				System.out.println("Invalid command. Use help to view commands");
-			}
-		}
-		s.close();
-	}
+
+private void count() {
+								MongoClientURI connectionString = new MongoClientURI("mongodb://ec2-13-59-38-216.us-east-2.compute.amazonaws.com:27018");
+								MongoClient mongo = new MongoClient(connectionString);
+								MongoDatabase database = mongo.getDatabase("testdb");
+								MongoCollection<Document> collection = database.getCollection("testCol");
+								System.out.println("Collection " +
+																											collection.getNamespace().getDatabaseName() +
+																											"." +
+																											collection.getNamespace().getCollectionName() +
+																											" has " +
+																											collection.countDocuments() +
+																											" records.");
+								mongo.close();
+}
+
+public static void main(String[] args) {
+								Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+								mongoLogger.setLevel(Level.SEVERE);
+
+								Main main = new Main();
+
+								System.out.println("Welcome to the Employee Compensation Console\n\n"
+																											+ "Version: 0.1\n\n"
+																											+ "Enter help for list of commands\n");
+
+								Scanner s = new Scanner(System.in);
+
+								while(true) {
+																System.out.print("command> ");
+																String input = s.next();
+																if(input.toLowerCase().trim().equals("help")) {
+																								System.out.println("exit - exit console");
+																								System.out.println("count - return the number of records on file");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																								System.out.println("? - ");
+																}
+																else if (input.toLowerCase().trim().equals("exit"))
+																								break;
+																else if(input.toLowerCase().trim().equals("count")) {
+																								main.count();
+																}
+																else {
+																								System.out.println("Invalid command. Use help to view commands");
+																}
+								}
+								s.close();
+}
 }
