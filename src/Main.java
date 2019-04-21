258d876d35b8bb76a2ffb0e9a100a85943989fb6
Grant <clegggrant@gmail.com>
2019-04-20 19:51:01 -0700

    Public Protection Function
    Finds and prints the employee id's of the first 1000 employees in public protection from 2017-2018

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import com.mongodb.Block;
 
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.FindIterable;
 import static com.mongodb.client.model.Filters.*;
 import com.mongodb.client.result.DeleteResult;
 import static com.mongodb.client.model.Updates.*;
@@ -38,6 +39,28 @@ public class Main {
 		mongo.close();
 	}
 
+	private void publicProtection() {
+		MongoClientURI connectionString = new MongoClientURI("mongodb://ec2-13-59-38-216.us-east-2.compute.amazonaws.com:27018");
+		MongoClient mongo = new MongoClient(connectionString);
+		MongoDatabase database = mongo.getDatabase("projectdb");
+		MongoCollection<Document> collection = database.getCollection("sfdata");
+
+		MongoCursor<Document> cursor = collection.find(and(eq("Organization_Group","Public Protection"),or(eq("Year",2018),eq("Year",2017)))).limit(1000).iterator();
+
+		try {
+			while(cursor.hasNext()) {
+				Document doc = cursor.next();
+				System.out.println("Employee Identifier: " + doc.get("Employee_Identifier") + "\n" +
+						"Organization Group: " + doc.get("Organization_Group") + "\n" +
+						"Year: " + doc.get("Year") + "\n");
+			}
+		}
+		finally {
+			cursor.close();
+			mongo.close();
+		}
+	}
+
 	public static void main(String[] args) {
 		Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
 		mongoLogger.setLevel(Level.SEVERE);
@@ -56,7 +79,7 @@ public class Main {
 			if(input.toLowerCase().trim().equals("help")) {
 				System.out.println("exit - exit console");
 				System.out.println("count - return the number of records on file");
-				System.out.println("? - ");
+				System.out.println("publicprotection - return employees from public protection from 2017-2018");
 				System.out.println("? - ");
 				System.out.println("? - ");
 				System.out.println("? - ");
@@ -75,6 +98,9 @@ public class Main {
 				break;
 			else if(input.toLowerCase().trim().equals("count")) {
 				main.count();
+			}
+			else if(input.toLowerCase().trim().equals("publicprotection")) {
+				main.publicProtection();
 			}
 			else if(input.isEmpty()) {
 
